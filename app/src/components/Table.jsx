c9e8bbfc29e581096f5bf6898edf9736dc8ab08c
smenaq <sergiosantosmenaq@gmail.com>
2023-03-21 10:19:06 -0500

    setup

---
+++ app/src/components/Table.jsx
@@ -0,0 +1,25 @@
+import styles from './Table.module.css'
+
+function Table () {
+    return (
+        <table className={styles.table}>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    )
+}
+
+export default Table
